202419fba4ffb647aee0b7f53569e9260527fe0b
Riya Rane <122218240+riyaaaarane@users.noreply.github.com>
2025-05-25 11:21:05 +0530

    Update download.sh

--- challenge-1/data/download.sh
+++ challenge-1/data/download.sh
@@ -1,1 +1,15 @@
+# Downloading a dataset in google-colab
+# === Example: Download a dataset === 
+pip install kaggle
 
+#Create kaggle api (kaggle.json)
+from google.colab import files
+files.upload()  # Upload kaggle.json
+
+!mkdir -p ~/.kaggle
+!mv "kaggle (2).json" ~/.kaggle/kaggle.json
+!chmod 600 ~/.kaggle/kaggle.json
+
+!kaggle competitions download -c soil-classification
+
+!unzip soil-classification.zip
